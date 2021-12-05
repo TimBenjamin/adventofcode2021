37a740c96ca654fa75008fa9def32058e02cccb8
Tim Benjamin <tim@timbenjamin.com>
2021-12-05 15:41:05 +0000

    done part 1

--- day_5/part_1.py
+++ day_5/part_1.py
@@ -1,5 +1,6 @@
 
 coords = []
+seenCoords = []
 multiples = 0
 
 def run(filename):
@@ -12,20 +13,20 @@ def run(filename):
                 y1 = int(y1)
                 y2 = int(y2)
                 if y1 > y2:
-                    for i in range((y1 - y2)+1):
-                        doCoord(x1 + "," + str(y2 + i))
+                    for i in range(y1, y2-1, -1):
+                        doCoord(x1 + "," + str(i))
                 else:
-                    for i in range((y2 - y1)+1):
-                        doCoord(x1 + "," + str(y1 + i))
+                    for i in range(y1, y2+1):
+                        doCoord(x1 + "," + str(i))
             elif y1 == y2:
                 x1 = int(x1)
                 x2 = int(x2)
                 if x1 > x2:
-                    for i in range((x1 - x2)+1):
-                        doCoord(str(x2 + i) + "," + y1)
+                    for i in range(x1, x2-1, -1):
+                        doCoord(str(i) + "," + y1)
                 else:
-                    for i in range((x2 - x1)+1):
-                        doCoord(str(x1 + i) + "," + y1)
+                    for i in range(x1, x2+1):
+                        doCoord(str(i) + "," + y1)
             else:
                 # ignore diagonals
                 continue
@@ -34,10 +35,12 @@ def doCoord(coord):
     global multiples
     global coords
     if coord in coords:
-        multiples += 1
+        if coord not in seenCoords:
+            multiples += 1
+            seenCoords.append(coord)
     else:
         coords.append(coord)
 
 
-run("input.txt")
+run("test_input.txt")
 print(multiples)
