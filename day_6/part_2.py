7b1cf63dcf2b679890770bc3ace4d364b1ac1baf
Tim Benjamin <tim@tienjin.com>
2021-12-06 18:08:19 +0000

    neater

--- day_6/part_2.py
+++ day_6/part_2.py
@@ -2,17 +2,7 @@ def getInput(filename):
     with open(filename) as file:
         line = file.readline().rstrip()
     population = line.split(",")
-    fish = {
-        0: 0,
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-        8: 0
-    }
+    fish = [0] * 9
     for p in population:
         fish[int(p)] += 1
     return fish
@@ -26,8 +16,4 @@ for day in range(256):
     fish[8] = numNew
     fish[6] += numNew
 
-total = 0
-for f in range(9):
-    total += fish[f]
-
-print(total)
+print(sum(fish))
